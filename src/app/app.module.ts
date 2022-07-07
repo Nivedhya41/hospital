5573efebe2ba7d672205ebddfc16124b3360a21f
Nivedhya41 <nivedhya61@gmail.com>
2022-07-08 00:01:38 +0530

    view-doctors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontPageComponent } from './front-page/front-page.component';
 import { SearchDeptComponent } from './search-dept/search-dept.component';
+import { ViewDoctorsComponent } from './view-doctors/view-doctors.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FrontPageComponent,
-    SearchDeptComponent
+    SearchDeptComponent,
+    ViewDoctorsComponent
   ],
   imports: [
     BrowserModule,
