d619fdbba1302db39c81a6387cec399c4989bcab
Nivedhya41 <nivedhya61@gmail.com>
2022-07-07 22:58:28 +0530

    search-dept

---
+++ src/app/search-dept/search-dept.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-dept',
+  templateUrl: './search-dept.component.html',
+  styleUrls: ['./search-dept.component.css']
+})
+export class SearchDeptComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
